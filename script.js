6b0b9121658271c871af7d031df0059eea1f17a8
Kathy Toufighi <kathy.toufighi@gmail.com>
2017-03-28 12:40:53 -0400

    optimizing script file to be more DRY

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var newLink = document.createElement("a");
 var allParagraphs = document.getElementsByTagName("p");
 var firstParagraph = allParagraphs[0];
-function revealCopy(e) {
+function toggleCopy(e) {
   var allParagraphs = document.getElementsByTagName("p");
   for(var i = 0; i < allParagraphs.length; i++) {
     if(i === 0) {
@@ -9,16 +9,19 @@ function revealCopy(e) {
     }
     allParagraphs[i].style.display = "block";
   }
-  this.remove();
-  e.preventDefault();
+  if(this === newLink) {
+    this.remove(); //refers to the element that intiated the event in this case Read More node inside the function revealCopy. by saying this.remove() it will remove the Read More link in the paragraph.
+  }
+  if(e !== undefined) {
+    e.preventDefault();
+  }
 }
 
 newLink.setAttribute("href", "#");
 newLink.setAttribute("class", "more-link");
 newLink.innerHTML = "Read more";
-newLink.addEventListener("click", revealCopy);
-// newLink.style.display = "inline-block";
-// newLink.style.marginLeft = "10px";
+newLink.addEventListener("click", toggleCopy);
+
 for(var i = 0; i < allParagraphs.length; i++) {
   if(i === 0) {
     continue;
