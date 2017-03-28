641914abf6bcc62ee56eec53f49c7edb0e6cbd56
Kathy Toufighi <kathy.toufighi@gmail.com>
2017-03-28 12:05:33 -0400

    finished writing the product Read More

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 var newLink = document.createElement("a");
 var allParagraphs = document.getElementsByTagName("p");
 var firstParagraph = allParagraphs[0];
+function revealCopy(e) {
+  var allParagraphs = document.getElementsByTagName("p");
+  for(var i = 0; i < allParagraphs.length; i++) {
+    if(i === 0) {
+      continue;
+    }
+    allParagraphs[i].style.display = "block";
+  }
+  this.remove();
+  e.preventDefault();
+}
 
 newLink.setAttribute("href", "#");
 newLink.setAttribute("class", "more-link");
 newLink.innerHTML = "Read more";
+newLink.addEventListener("click", revealCopy);
 // newLink.style.display = "inline-block";
 // newLink.style.marginLeft = "10px";
 for(var i = 0; i < allParagraphs.length; i++) {
@@ -12,7 +24,6 @@ for(var i = 0; i < allParagraphs.length; i++) {
     continue;
   }
   allParagraphs[i].style.display = "none";
-
 }
 
 firstParagraph.appendChild(newLink);
