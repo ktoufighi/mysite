4f84fa3b0cac6cd1f3637f46032698c33414c60d
Kathy Toufighi <kathy.toufighi@gmail.com>
2017-03-28 13:58:46 -0400

    this portion of JS is done

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
-var newLink = document.createElement("a");
-var allParagraphs = document.getElementsByTagName("p");
-var firstParagraph = allParagraphs[0];
-function toggleCopy(e) {
+(function() {
+  var newLink = document.createElement("a");
   var allParagraphs = document.getElementsByTagName("p");
-  for(var i = 0; i < allParagraphs.length; i++) {
-    if(i === 0) {
-      continue;
+  var firstParagraph = allParagraphs[0];
+  function toggleCopy(e) {
+    var allParagraphs = document.getElementsByTagName("p");
+    for(var i = 0; i < allParagraphs.length; i++) {
+      var para = allParagraphs[i];
+      if(i === 0) {
+        continue;
+      }
+      if (para.style.display === "none"
+      ){
+        para.style.display= "block";
+      }
+      else {
+        para.style.display = "none";
+      }
     }
-    allParagraphs[i].style.display = "block";
+    if(this === newLink) {
+      this.remove(); //refers to the element that intiated the event in this case Read More node inside the function revealCopy. by saying this.remove() it will remove the Read More link in the paragraph.
+    }
+    if(e !== undefined && e.preventDefault !== undefined) {
+      e.preventDefault();
+    }
   }
-  if(this === newLink) {
-    this.remove(); //refers to the element that intiated the event in this case Read More node inside the function revealCopy. by saying this.remove() it will remove the Read More link in the paragraph.
-  }
-  if(e !== undefined) {
-    e.preventDefault();
-  }
-}
 
-newLink.setAttribute("href", "#");
-newLink.setAttribute("class", "more-link");
-newLink.innerHTML = "Read more";
-newLink.addEventListener("click", toggleCopy);
+  newLink.setAttribute("href", "#");
+  newLink.setAttribute("class", "more-link");
+  newLink.innerHTML = "Read more";
+  newLink.addEventListener("click", toggleCopy);
+  toggleCopy();
 
-for(var i = 0; i < allParagraphs.length; i++) {
-  if(i === 0) {
-    continue;
-  }
-  allParagraphs[i].style.display = "none";
-}
+  // for(var i = 0; i < allParagraphs.length; i++) {
+  //   if(i === 0) {
+  //     continue;
+  //   }
+  //   para.style.display = "none";
+  // }
 
-firstParagraph.appendChild(newLink);
+  firstParagraph.appendChild(newLink);
+}());
