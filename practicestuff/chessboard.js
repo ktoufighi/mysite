61c9c6326438d8d0cdcb206d997d99451aa39207
Kathy Toufighi <kathy.toufighi@gmail.com>
2017-03-29 11:23:11 -0400

    chessboard game grid in JS

---
+++ practicestuff/chessboard.js
@@ -0,0 +1,45 @@
+// Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
+// Passing this string to console.log should show something like this:
+//
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+// When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+// The string can be built by starting with an empty one ("") and repeatedly adding characters. A newline character is written "\n".
+//
+// Use console.log to inspect the output of your program.
+//
+// To work with two dimensions, you will need a loop inside of a loop. Put curly braces around the bodies of both loops to make it easy to see where they start and end. Try to properly indent these bodies. The order of the loops must follow the order in which we build up the string (line by line, left to right, top to bottom). So the outer loop handles the lines and the inner loop handles the characters on a line.
+//
+// You’ll need two variables to track your progress. To know whether to put a space or a hash sign at a given position, you could test whether the sum of the two counters is even (% 2).
+//
+// Terminating a line by adding a newline character happens after the line has been built up, so do this after the inner loop but inside of the outer loop.
+
+
+// -----------------------------
+// CODE HERE
+// -----------------------------
+
+var chessBoard = function(size) {
+  var hash = "#";
+  var space = " ";
+
+  for (var i = 0; i < size; i ++) {
+
+     hash += '\n'
+     for (var j = 0; j < size; j ++) {
+       if((i + j) % 3 == 0) {
+         hash += " ";
+       }
+       else {
+         hash += "#";
+       }
+     };
+  };
+  console.log(hash)
+} (15)
